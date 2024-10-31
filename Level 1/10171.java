684448598d2795bbe3933c0a709c37bb14e55d02
Kimeloper <sesame.kim321@gmail.com>
2024-10-31 14:51:11 +0900

    Create 10171 고양이.java
    [Bronze V]
    🐈
    https://www.acmicpc.net/problem/10171

---
+++ Level 1/10171.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+        // '\'는 이스케이프 문자여서 단독 출력이 불가능하므로 '\\' 이렇게 두번 써야한다.
+    }
+}
